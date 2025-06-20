308695f59e3981699b8166db129cbc19f06df11f
Guilherme Sartori <sartori.guilherme@gmail.com>
2025-06-20 07:06:43 -0300

    Adicionar o desafio 00

---
+++ exemplo_00.py
@@ -0,0 +1,8 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+#print(int(input("Digite o primeiro numero: ")) + int(input("Digite seu segundo numero: ")))
+
+numero = 3 #int
+print(type(numero))
+nome_de_usuario = "Guilherme" #string
+numero_decimal = 4.78 #float
+verdadeiro = True #bool
